b16dfd0035eb4d5fa595b34218ea7016de961c29
Henry Davies <henrycdavies@icloud.com>
2024-07-17 13:05:53 +0100

    save

--- src/commands/sync.rs
+++ src/commands/sync.rs
@@ -1,7 +1,7 @@
 use clap::Args;
-use git2::{Config, Cred, RemoteCallbacks};
+use git2::{Config, Cred, FetchOptions, RemoteCallbacks};
 
-use super::{checkout::open_repo, result::Result};
+use super::{checkout::open_repo, error::ErrorKind, result::Result, trunk::find_trunk_branch, Error};
 
 #[derive(Args, Debug)]
 pub struct SyncCommandArgs {}
@@ -9,7 +9,7 @@ pub struct SyncCommandArgs {}
 pub fn sync(args: &SyncCommandArgs) -> Result<()> {
     let repo = open_repo()?;
 
-    // Define the remote callbacks
+    // Configure auth & progress reporting
     let mut callbacks = RemoteCallbacks::new();
     callbacks.credentials(|url, username_from_url, allowed_types| {
         let config = Config::open_default()?;
@@ -35,10 +35,46 @@ pub fn sync(args: &SyncCommandArgs) -> Result<()> {
         true
     });
     
-    // Fetch from remote
-    let branch_refspecs = ["refs/heads/*:refs/remotes/origin/*"];
-    let mut fetch_options = git2::FetchOptions::new();
+    // Configure remote & fetch config
     let mut remote = repo.find_remote("origin")?;
+    let branch_refspecs = ["refs/heads/*:refs/remotes/origin/*"];
+    let mut fetch_options = FetchOptions::new();
+    fetch_options.remote_callbacks(callbacks);
+    fetch_options.prune(git2::FetchPrune::On);
+
+    // Fetch
     remote.fetch(&branch_refspecs, Some(&mut fetch_options), None)?;
+
+    // Merge fetched changes
+    let fetch_head = repo.find_reference("FETCH_HEAD")?;
+    let fetch_commit = repo.reference_to_annotated_commit(&fetch_head)?;
+    let (analysis, _) = repo.merge_analysis(&[&fetch_commit])?;
+
+    let trunk_branch = find_trunk_branch(&repo)?;
+    let trunk_branch_refname = format!("refs/heads/{}", trunk_branch);
+    if analysis.is_fast_forward() {
+        // Fast forward merg
+        let mut reference = repo.find_reference(trunk_branch_refname.as_str())?;
+        reference.set_target(fetch_commit.id(), "Fast-forward")?;
+        repo.set_head(trunk_branch_refname.as_str())?;
+        repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))?;
+    } else if analysis.is_normal() {
+        // Normal merge
+        let commit = repo.find_commit(fetch_commit.id())?;
+        let mut index = repo.merge_commits(&repo.head()?.peel_to_commit()?, &commit, None)?;
+        if index.has_conflicts() {
+            return Err(Error::new(
+                ErrorKind::GitError,
+                "Merge conflict".to_string(),
+            ));
+        }
+        let result_tree = repo.find_tree(index.write_tree_to(&repo)?)?;
+        let signature = repo.signature()?;
+        let parent_commit = repo.head()?.peel_to_commit()?;
+        repo.commit(Some("HEAD"), &signature, &signature, "Merge commit", &result_tree, &[&parent_commit, &fetch_commit])?;
+        repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force()))?;
+    } else {
+        // Merge conflict
+    }
     return Ok(())
 }
